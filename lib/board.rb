f32e8e2f2cee3374d7c84d1ee8a9f3334dacf1d4
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-10 08:31:07 -0600

    Work on valid_placement method

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,27 @@ class Board
     @cells = build_grid
   end
 
+  def valid_placement?(ship, coordinates)
+    letter_by_itself = coordinates.map { |coordinate| coordinate[0] }
+    number_by_itself = coordinates.map { |coordinate| coordinate[1] }
+    letter_byself_same_value = if letter_by_itself.uniq.length == letter_by_itself.length
+    end
+    number_byself_same_value = if number_by_itself.uniq.length == number_by_itself.length
+    end
+    letter_order = ("A".."D").each_cons(ship.length) { |letter| p letter }
+    number_order = (1..4).each_cons(ship.length) { |number| p number }
+
+    if coordinates.length == ship.length
+      # if the number of coordinates is equal to ship's length
+      if letter_byself_same_value && (number_by_itself == number_order.any?)
+      #if the letters are the same and the numbers are consecutive
+    elsif (letter_by_itself == letter_order.any?) && number_byself_same_value
+      # if the letters are consecutive and the numbers are the same
+      else false
+      end
+    end
+  end
+
   def validate_coordinate?(coordinate)
     @cells.keys.any? do |key|
       key == coordinate
@@ -12,7 +33,7 @@ class Board
   end
 
   def build_grid
-    grid = Hash.new
+    grid = {}
     ('A'..'D').each do |letter|
       (1..4).each do |num|
         coordinates = "#{letter}#{num}"
