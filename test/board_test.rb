94a25e7ad62b2bfc197b4646b648aff909b0e00f
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-11 13:39:06 -0600

    Add placing ship test

--- test/board_test.rb
+++ test/board_test.rb
@@ -11,6 +11,7 @@ class BoardTest < MiniTest::Test
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
+
   end
 
   def test_board_exists
@@ -40,6 +41,18 @@ class BoardTest < MiniTest::Test
     refute @board.valid_placement?(@submarine, ["C2", "D3"])
     assert @board.valid_placement?(@submarine, ["A1", "A2"])
     assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_placing_ship
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
+    assert cell_3.ship == cell_2.ship
   end
 
 
