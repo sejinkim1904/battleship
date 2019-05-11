50d15c72611b811cddaf752077180dd49b63272f
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-10 18:02:48 -0600

    Add consecutive and diagonal tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -29,11 +29,17 @@ class BoardTest < MiniTest::Test
 
   def test_ship_has_valid_placement
 
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
     refute @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
-    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
     refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-
+    refute @board.valid_placement?(@submarine, ["A1", "C1"])
+    refute @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    refute @board.valid_placement?(@submarine, ["C1", "B1"])
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@submarine, ["C2", "D3"])
+    assert @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
 
